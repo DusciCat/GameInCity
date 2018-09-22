bad794fa9005696ec5c9929fb28f96e9bae6f822
DusciCat <duscicat@gmail.com>
2018-09-22 20:37:36 +0700

    merged map with node js backend and add 3 buttons using mdb cdn

--- jsBackend/todo-app/index.js
+++ jsBackend/todo-app/index.js
@@ -8,41 +8,19 @@ app.set("view engine", "ejs");
 //render css files
 app.use(express.static("public"));
 
-//placeholders for added task
-var task = ["buy socks", "practise with nodejs"];
-//placeholders for removed task
-var complete = ["finish jquery"];
-
-//post route for adding new task 
-app.post("/addtask", function(req, res) {
-    var newTask = req.body.newtask;
-    //add the new task from the post route
-    task.push(newTask);
-    res.redirect("/");
-});
-
-app.post("/removetask", function(req, res) {
-    var completeTask = req.body.check;
-    //check for the "typeof" the different completed task, then add into the complete task
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        //check if the completed task already exits in the task when checked, then remove it
-        task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
-        }
-    }
-    res.redirect("/");
-});
 
 //render the ejs and display added task, completed task
 app.get("/", function(req, res) {
-    res.render("index", { task: task, complete: complete });
+    // res.render("index");
+    res.sendFile( __dirname + "/public/" + "index.html" );
 });
 
-//set app to listen on port 3000
+//render the ejs and display added task, completed task
+app.get("/cityIds", function(req, res) {
+    res.send([{id: "198100709"}]);
+});
+
+
 app.listen(3000, function() {
     console.log("server is running on port 3000");
 });
---
+++ jsBackend/todo-app/public/scripts/script.js
@@ -0,0 +1,103 @@
+var points;
+var center;
+var myMap;
+
+ymaps.ready(getCityIds);
+
+function initMap() {
+    myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчнию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [center[1], center[0]],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 9
+    });
+}
+
+function drawPolygon() {
+
+    // Создаем многоугольник, используя класс GeoObject.
+    var myGeoObject = new ymaps.GeoObject({
+        // Описываем геометрию геообъекта.
+        geometry: {
+            // Тип геометрии - "Многоугольник".
+            type: "Polygon",
+            // Указываем координаты вершин многоугольника.
+            coordinates: [
+                points.map(function (x) { return [x[1], x[0]] })
+            ],
+            // Задаем правило заливки внутренних контуров по алгоритму "nonZero".
+            fillRule: "nonZero"
+        },
+        // Описываем свойства геообъекта.
+        properties: {
+            // Содержимое балуна.
+            balloonContent: "Многоугольник"
+        }
+    }, {
+            // Описываем опции геообъекта.
+            // Цвет заливки.
+            fillColor: '#00FF00',
+            // Цвет обводки.
+            strokeColor: '#0000FF',
+            // Общая прозрачность (как для заливки, так и для обводки).
+            opacity: 0.5,
+            // Ширина обводки.
+            strokeWidth: 5,
+            // Стиль обводки.
+            strokeStyle: 'shortdash'
+        });
+
+    myGeoObject.events.add(['click'], function (e) {
+        alert(42);
+    });
+
+    // Добавляем многоугольник на карту.
+    myMap.geoObjects.add(myGeoObject);
+}
+
+function getCityIds() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "/cityIds", true);
+    request.onreadystatechange = function (aEvt) {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                var data = JSON.parse(request.responseText);
+                var id = data[0].id;
+
+                getCenterCoordinates(id);
+            }
+            else {
+                alert("Error loading city ids\n");
+            }
+        }
+    };
+
+    request.send();
+}
+
+function getCenterCoordinates(id) {
+
+    var request = new XMLHttpRequest();
+    request.open("GET", `https://nominatim.openstreetmap.org/details.php?place_id=${id}&polygon_geojson=1&format=json`, true);
+    request.onreadystatechange = function (aEvt) {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                var data = JSON.parse(request.responseText);
+                points = data.geometry.coordinates[0];
+                center = data.centroid.coordinates;
+
+                initMap();
+                drawPolygon();
+            }
+            else {
+                alert("Error loading page\n");
+            }
+        }
+    };
+
+    request.send();
+}   
